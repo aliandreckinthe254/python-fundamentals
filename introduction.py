dcd03d9d0aa9d52c7b9b21667098239db0fb7224
aliandreckinthe254 <iantekiba@gmail.com>
2025-02-03 13:16:40 +0300

    initial

---
+++ introduction.py
@@ -0,0 +1,10 @@
+#print is a method used to display values
+#start
+print("Ian")
+print("eMobilis")
+print(39)
+print(100*2)
+#waaadup
+x = 2
+y = 5
+print(x*y)
---
+++ variables.py
@@ -0,0 +1,16 @@
+#Variable is a container used to store a value
+number = 10
+fname = "Ian"
+lname= "kibai"
+language= "python"
+a=10
+b=15
+
+
+print(number)
+print(fname,lname)
+
+print(language, "is a web-programming language.")
+print(a+b)
+
+print("The sum of",a,"and",b,"is",a+b)
