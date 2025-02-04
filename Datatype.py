de5ebba9f5708b0199918cb37a9124b4655e7d91
aliandreckinthe254 <iantekiba@gmail.com>
2025-02-04 13:15:06 +0300

    initial

---
+++ Datatype.py
@@ -0,0 +1,34 @@
+number=45 #Int
+num=66.78 #Float
+text="Hello" #string
+ismammal=True #Boolean
+
+#Data structures
+cars= ["Toyota","Mercedes","Ford"] #this is a list...
+fruits= ("Banana","Apples","Grapes") #Tuple
+countries= {"England","Italy","Japan"} #set
+details={
+    "firstname": "Brian",
+    "course": "MIT",
+    "age": 19,
+    "marital status":"single"
+
+}
+
+
+
+print(number)
+print(num)
+print(text)
+print(ismammal)
+print(cars)
+print(fruits)
+print(countries)
+print("Brians course:",details["course"])
+
+
+#Typecasting coverts data
+print(float(number))
+print(int(num))
+
+
---
+++ string.py
@@ -0,0 +1,31 @@
+text = "Hello world"
+language ="PYTHON"
+greeting="Hello"
+name="Ian"
+
+
+
+print(text)
+print(language)
+
+#Accesing an element in a string
+print(text[0])
+print(text[1])
+
+#size of a string
+print(len(text))
+
+#modifying a string
+print(text.upper())
+print(language.lower())
+
+#string Concatenation-- joining strings
+print(greeting+name)
+print(greeting +" "+name)
+
+
+
+
+
+
+
